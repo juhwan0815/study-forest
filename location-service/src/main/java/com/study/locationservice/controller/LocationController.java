51850629bc8dcfa999f962405e4b6d26b52ae8c3
juhwan0815 <juwom0831@naver.com>
2021-06-23 01:28:27 +0900

    refactor: location-service cors 설정 제거

--- location-service/src/main/java/com/study/locationservice/controller/LocationController.java
+++ location-service/src/main/java/com/study/locationservice/controller/LocationController.java
@@ -18,7 +18,6 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 public class LocationController {
