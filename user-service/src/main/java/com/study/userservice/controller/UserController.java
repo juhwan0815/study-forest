b0f490b234a3ed455d7651e80a6dc54ad705234d
juhwan0815 <juwom0831@naver.com>
2021-06-23 01:29:26 +0900

    refactor: user-service cors 설정 제거

--- user-service/src/main/java/com/study/userservice/controller/UserController.java
+++ user-service/src/main/java/com/study/userservice/controller/UserController.java
@@ -20,7 +20,6 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
