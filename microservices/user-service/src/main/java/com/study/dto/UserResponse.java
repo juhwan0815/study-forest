2e68f411eef00ad60c92ec8e09aee980765ed043
juhwan0815 <juwom0831@naver.com>
2021-12-19 23:40:40 +0900

    fix : user-service UserResponse 수정

--- microservices/user-service/src/main/java/com/study/dto/UserResponse.java
+++ microservices/user-service/src/main/java/com/study/dto/UserResponse.java
@@ -1,6 +1,7 @@
 package com.study.dto;
 
 import com.study.domain.User;
+import com.study.domain.UserRole;
 import com.study.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,8 @@ public class UserResponse {
 
     private Long userId; // 회원 ID
 
+    private UserRole role; // 유저 권한
+
     private String nickName; // 닉네임
 
     private String ageRange; // 나이대
@@ -20,6 +23,6 @@ public class UserResponse {
     private String imageUrl; // 이미지
 
     public static UserResponse from(User user) {
-        return new UserResponse(user.getId(), user.getNickName(), user.getAgeRange(), user.getImage().getImageUrl());
+        return new UserResponse(user.getId(), user.getRole(), user.getNickName(), user.getAgeRange(), user.getImage().getImageUrl());
     }
 }
