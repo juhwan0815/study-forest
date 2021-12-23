645f020e4a18624e1845e126b6f3c9c4acfd3793
juhwan0815 <juwom0831@naver.com>
2021-12-23 11:56:48 +0900

    feat : user-service User 생성자 추가

--- microservices/user-service/src/main/java/com/study/domain/User.java
+++ microservices/user-service/src/main/java/com/study/domain/User.java
@@ -1,6 +1,7 @@
 package com.study.domain;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User extends BaseEntity {
 
     @Id
